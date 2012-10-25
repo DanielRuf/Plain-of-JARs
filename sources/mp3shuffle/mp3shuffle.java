2cac292e80e71d1040c675a6ac413dfa15fa109c
Daniel Ruf <daniel@daniel-ruf.de>
2012-10-25 20:50:52 +0200

    mp3shuffle 1.1.0

--- sources/mp3shuffle/mp3shuffle.java
+++ sources/mp3shuffle/mp3shuffle.java
@@ -2,7 +2,7 @@
   *
   * MP3SHUFFLE
   *
-  * @version 1.0.3 vom 24.10.2012
+  * @version 1.1.0 vom 25.10.2012
   * @Daniel Ruf 
   */
 import java.io.IOException;
@@ -12,12 +12,13 @@ import java.io.File;
 import java.util.Collections;
 import java.util.UUID;
 import java.util.ArrayList;
+import java.util.Date;
 public class mp3shuffle {
   public static void main(String args[]) throws Exception {
     String path2 = ".";
     File directory = new File(path2);    
     File[] myarray;
-    String version = "1.0.3";
+    String version = "1.1.0";
     String program = "MP3SHUFFLE";  
     System.out.println(program + " " + version );
     myarray=directory.listFiles(new FileFilter() {
@@ -27,9 +28,11 @@ public class mp3shuffle {
     });
     System.out.println("Found " + myarray.length + " file(s)");
     ArrayList<Integer> numbers = new ArrayList<Integer>();
+    ArrayList<Long> dates = new ArrayList<Long>();
     for(int i = 0; i < myarray.length; i++)
     {
       numbers.add(i+1);
+      dates.add(new Date().getTime());
     }
     Collections.shuffle(numbers); 
     for (int j = 0; j < myarray.length; j++)
@@ -37,6 +40,7 @@ public class mp3shuffle {
       int file_number = j+1;
       System.out.print("\rProcessing file " + file_number + " of " + myarray.length);
       File path=myarray[j];
+      path.setLastModified(dates.get(j));
       String path_current = path.toString();  
       UUID timestamp = UUID.randomUUID();      
       String new_filename = numbers.get(j) + "_" + timestamp + ".mp3"; 
