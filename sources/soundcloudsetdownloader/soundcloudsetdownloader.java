64d61abb166bcbc6329e4d663901070c4787a7b4
Daniel Ruf <daniel@daniel-ruf.de>
2013-02-20 21:25:20 +0100

    soundcloudsetdownloader 1.1.0 and soundcloudtrackdownloader 1.1.0

--- sources/soundcloudsetdownloader/soundcloudsetdownloader.java
+++ sources/soundcloudsetdownloader/soundcloudsetdownloader.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.lang.String.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.*;
@@ -14,20 +16,25 @@ import java.io.Console;
 import java.net.URL;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import java.lang.String.*;   
 import org.json.JSONException;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.ID3v23Tag;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.NotSupportedException;
+import com.mpatric.mp3agic.UnsupportedTagException;
 
 /**
   *
   * SoundCloudSetDownloader
   *
-  * @version 1.0.1 vom 15.02.2013
+  * @version 1.1.0 vom 20.02.2013
   * @author 
   */
 
 public class soundcloudsetdownloader {
   public static void main(String[] args) throws Exception {
-    String version = "1.0.1";
+    String version = "1.1.0";
     String program = "SoundCloudSetDownloader";
     System.out.println(program + " " + version );
     String client_id = "b45b1aa10f1ac2941910a7f0d10f8e28";
@@ -70,7 +77,9 @@ public class soundcloudsetdownloader {
         //int file_size = conn.getContentLength();
         String content_length= conn.getHeaderField( "content-length" );
         InputStream is = conn.getInputStream();
-        OutputStream outstream = new FileOutputStream(new File(file_number+"_"+username_soundcloud+" - "+playlist_name+" - "+title_track+".mp3"));
+        String mp3_filename = file_number+"_"+username_soundcloud+" - "+title_track+".mp3";
+        String mp3_id3filename = file_number+"_"+username_soundcloud+" - "+title_track+"_id3.mp3";
+        OutputStream outstream = new FileOutputStream(new File(mp3_filename));
         long fileSize = Long.valueOf(content_length).longValue();
         long bytesRead = 0;
         int percentage = -1;
@@ -87,6 +96,25 @@ public class soundcloudsetdownloader {
           System.out.print("\rDownloading track "+j_real+" of "+tracks_count+" from playlist "+i_real+" of "+playlists_count+" "+n_perct+"");
         }
         outstream.close();
+        Mp3File mp3file = new Mp3File(mp3_filename);
+        ID3v2 id3v2Tag;
+        if (mp3file.hasId3v2Tag()) {
+          id3v2Tag = mp3file.getId3v2Tag();
+        } else {
+          // mp3 does not have an ID3v2 tag, let's create one..
+          id3v2Tag = new ID3v23Tag();
+          mp3file.setId3v2Tag(id3v2Tag);
+        }
+        id3v2Tag.setArtist(username_soundcloud);
+        id3v2Tag.setTitle(title_track);
+        id3v2Tag.setAlbum(playlist_name);
+        id3v2Tag.setYear(tracks.getJSONObject(j).get("release_year").toString());
+        id3v2Tag.setComment(tracks.getJSONObject(j).getString("description"));
+        mp3file.save(mp3_id3filename);
+        File file_id3 = new File(mp3_id3filename);
+        File old_mp3file = new File(mp3_filename);
+        old_mp3file.delete();
+        file_id3.renameTo(new File(mp3_filename));
         j++;
       }
       i++;
--- sources/soundcloudtrackdownloader/soundcloudtrackdownloader.java
+++ sources/soundcloudtrackdownloader/soundcloudtrackdownloader.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.lang.String.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.*;
@@ -14,20 +16,25 @@ import java.io.Console;
 import java.net.URL;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import java.lang.String.*;   
 import org.json.JSONException;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.ID3v23Tag;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.NotSupportedException;
+import com.mpatric.mp3agic.UnsupportedTagException;
 
 /**
   *
   * SoundCloudTrackDownloader
   *
-  * @version 1.0.1 vom 15.02.2013
+  * @version 1.1.0 vom 20.02.2013
   * @author 
   */
 
 public class soundcloudtrackdownloader {
   public static void main(String[] args) throws Exception {
-    String version = "1.0.1";
+    String version = "1.1.0";
     String program = "SoundCloudTrackDownloader";
     System.out.println(program + " " + version );
     String client_id = "b45b1aa10f1ac2941910a7f0d10f8e28";
@@ -64,7 +71,9 @@ public class soundcloudtrackdownloader {
       //int file_size = conn.getContentLength();
       String content_length= conn.getHeaderField( "content-length" );
       InputStream is = conn.getInputStream();
-      OutputStream outstream = new FileOutputStream(new File(file_number+"_"+username_soundcloud+" - "+title_track+".mp3"));
+      String mp3_filename = file_number+"_"+username_soundcloud+" - "+title_track+".mp3";
+      String mp3_id3filename = file_number+"_"+username_soundcloud+" - "+title_track+"_id3.mp3";
+      OutputStream outstream = new FileOutputStream(new File(mp3_filename));
       long fileSize = Long.valueOf(content_length).longValue();
       long bytesRead = 0;
       int percentage = -1;
@@ -80,6 +89,25 @@ public class soundcloudtrackdownloader {
         System.out.print("\rDownloading track "+i_real+" of "+tracks_count+" "+n_perct+"");
       }
       outstream.close();
+      Mp3File mp3file = new Mp3File(mp3_filename);
+      ID3v2 id3v2Tag;
+      if (mp3file.hasId3v2Tag()) {
+        id3v2Tag = mp3file.getId3v2Tag();
+      } else {
+        // mp3 does not have an ID3v2 tag, let's create one..
+        id3v2Tag = new ID3v23Tag();
+        mp3file.setId3v2Tag(id3v2Tag);
+      }
+      id3v2Tag.setArtist(username_soundcloud);
+      id3v2Tag.setTitle(title_track);
+      //id3v2Tag.setAlbum("The Album");
+      id3v2Tag.setYear(tracks.getJSONObject(i).get("release_year").toString());
+      id3v2Tag.setComment(tracks.getJSONObject(i).getString("description"));
+      mp3file.save(mp3_id3filename);
+      File file_id3 = new File(mp3_id3filename);
+      File old_mp3file = new File(mp3_filename);
+      old_mp3file.delete();
+      file_id3.renameTo(new File(mp3_filename));
       i++;
     } 
     System.out.println("");
